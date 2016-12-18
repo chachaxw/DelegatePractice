32a9379f82008a50ae35af6057534dc7d516ebb4
Wei Zhou <867571123@qq.com>
2016-12-18 23:01:45 +0800

    添加代理模式传值

--- DelegatePractice/ReceivingVC.swift
+++ DelegatePractice/ReceivingVC.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ReceivingVC: UIViewController {
+class ReceivingVC: UIViewController, DataSendDelegate {
 
+    @IBOutlet weak var receivingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func userDidEnterData(data: String) {
+        receivingLabel.text = data
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showSendingVC" {
+            let sendingVC: SendingVC = segue.destination as! SendingVC
+            sendingVC.delegate = self
+        }
+    }
 }
 
--- DelegatePractice/SendingVC.swift
+++ DelegatePractice/SendingVC.swift
@@ -8,28 +8,30 @@
 
 import UIKit
 
-class SendingVC: UIViewController {
+protocol DataSendDelegate {
+    func userDidEnterData(data: String)
+}
 
+class SendingVC: UIViewController {
+    
+    @IBOutlet weak var dataEntryTextField: UITextField!
+
+    var delegate: DataSendDelegate? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendBtnWasPressed(_ sender: AnyObject) {
+        if delegate != nil {
+            if dataEntryTextField.text != nil {
+                let data = dataEntryTextField.text
+                delegate?.userDidEnterData(data: data!)
+                dismiss(animated: true, completion: nil)
+            }
+        }
+        
     }
-    */
-
 }
